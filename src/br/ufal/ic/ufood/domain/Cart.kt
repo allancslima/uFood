181c2c61537eb1975f45b5415295ff7c475202e5
Allan Lima <allan.lima@ilhasoft.com.br>
2019-12-03 22:44:43 -0300

    Add cart model

---
+++ src/br/ufal/ic/ufood/domain/Cart.kt
@@ -0,0 +1,45 @@
+package br.ufal.ic.ufood.domain
+
+class Cart {
+
+    private val foods: MutableMap<Food, Int> by lazy {
+        HashMap<Food, Int>()
+    }
+
+    @Throws(IllegalArgumentException::class)
+    fun add(food: Food, quantity: Int) {
+        if (quantity < 1) {
+            throw IllegalArgumentException("Invalid quantity.")
+        }
+        val currentQuantity = foods[food]
+
+        foods[food] = if (currentQuantity == null) {
+            quantity
+        } else {
+            currentQuantity + quantity
+        }
+    }
+
+    @Throws(IllegalArgumentException::class)
+    fun remove(food: Food, quantity: Int) {
+        if (quantity < 1) {
+            throw IllegalArgumentException("Invalid quantity.")
+        }
+        val currentQuantity = foods[food]
+
+        if (currentQuantity != null) {
+            val newQuantity = currentQuantity - quantity
+
+            if (newQuantity < 1) {
+                foods.remove(food)
+            } else {
+                foods[food] = newQuantity
+            }
+        }
+    }
+
+    fun getTotalPrice(): Double {
+        return foods.asIterable().sumByDouble { it.key.price * it.value }
+    }
+
+}
